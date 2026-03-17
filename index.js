19b8d14ddd5b590fb2319a98fc16ec75c0061d40
Ankit Sharma <ankit0525252@gmail.com>
2026-03-17 13:13:01 +0530

    gauge counting in prometheus

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ const requestCounter = new promClient.Counter({
   labelNames : ['methods', 'route', 'statuscode']
 })
 
+const activeRequestsGauge = new promClient.Gauge({
+  name : "active_http_requests",
+  help : "Number of active HTTP requests"
+})
+
 const middleware = (req, res, next)=>{
+  activeRequestsGauge.inc();
   const starttime = Date.now();
    
   res.on("finish", ()=>{
     const endtime = Date.now();
     console.log(`Request took ${endtime - starttime} ms`);
     requestCounter.inc({ methods: req.method, route: req.path, statuscode: res.statusCode });
+    activeRequestsGauge.dec();
   })
 
   next();
@@ -25,12 +32,8 @@ app.use(middleware);
 
 
 
-app.get("/cpu", (req, res)=>{
-  const cpuUsage = process.cpuUsage();
-  let cpuUsageAfter;
-  for(let i = 0; i < 1e7; i++) {
-     cpuUsageAfter = process.cpuUsage(cpuUsage);
-  }
+app.get("/cpu",async (req, res)=>{
+  await new Promise(resolve=> setTimeout(resolve, 10000))
 
   res.json({ cpuUsage: cpuUsageAfter });
 })
@@ -47,6 +50,7 @@ app.get("/metrics", async (req, res)=>{
   res.set('Content-Type', promClient.register.contentType);
   res.send(metrics);
 })
+
 app.listen(3000, ()=>{
   console.log("server is listening on port 3000");
 })
